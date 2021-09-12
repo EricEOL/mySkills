60d931ae903ca8c95a8f3cf82fd9e017c88b6e3e
EricEOL <lopes.eric051@gmail.com>
2021-09-12 16:56:10 -0300

    Inserting skills in state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,57 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput as Input } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput as Input,
+  TouchableOpacity
+} from 'react-native';
 
 export function Home() {
+
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill() {
+    setMySkills([...mySkills, newSkill]);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, Eric</Text>
 
-      <Input 
+      <Input
         style={styles.input}
+        placeholder="Insert a new skill"
+        placeholderTextColor="#444"
+        onChangeText={setNewSkill}
       />
+
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+        onPress={handleAddNewSkill}
+      >
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
+
+      {mySkills.map((skill, index) => (
+        <TouchableOpacity
+          key={index}
+          style={styles.buttonSkill}
+        >
+          <Text style={styles.textSkill}>{skill}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
     backgroundColor: '#121015',
     paddingHorizontal: 30,
     paddingVertical: 70
@@ -32,5 +68,29 @@ const styles = StyleSheet.create({
     padding: 15,
     marginTop: 30,
     borderRadius: 8
+  },
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 10
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold'
+  },
+  buttonSkill: {
+    backgroundColor: '#1f1e25',
+    padding: 15,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  textSkill: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
   }
 })
